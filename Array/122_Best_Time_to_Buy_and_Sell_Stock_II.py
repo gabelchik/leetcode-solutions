bd982dd38d59fd694608da8f014eeb0e60b8e6ac
gabelchik <lensuperhero@gmail.com>
2025-07-28 22:20:47 +0300

    122_Best_Time_to_Buy_and_Sell_Stock_II.py

--- Array/122_Best_Time_to_Buy_and_Sell_Stock_II.py
+++ Array/122_Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -11,24 +11,22 @@ class Solution:
 
 
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
 
-        
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-#         if not prices:
-#             return 0
-#
-#         (flag, profit) = (-1, 0)
-#         for i in range(len(prices) - 1):
-#             if flag == -1:
-#                 if prices[i+1] > prices[i]:
-#                     flag = i
-#                     if prices[i+1] - prices[flag] > 0 :
-#                         profit += prices[i+1] - prices[flag]
-#                         flag = -1
-#             else:
-#                 if prices[i + 1] - prices[flag] >= 0:
-#                     profit += prices[i + 1] - prices[flag]
-#                     flag = -1
-#
-#         return profit
+        (flag, profit) = (-1, 0)
+        for i in range(len(prices) - 1):
+            if flag == -1:
+                if prices[i+1] > prices[i]:
+                    flag = i
+                    if prices[i+1] - prices[flag] > 0 :
+                        profit += prices[i+1] - prices[flag]
+                        flag = -1
+            else:
+                if prices[i + 1] - prices[flag] >= 0:
+                    profit += prices[i + 1] - prices[flag]
+                    flag = -1
+
+        return profit
