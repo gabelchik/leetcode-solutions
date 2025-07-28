40ea6ec6b8af4e4c2cdb1b69bd55edc2d1150531
gabelchik <lensuperhero@gmail.com>
2025-07-28 22:22:07 +0300

    66_Plus_One.py

--- Array/66_Plus_One.py
+++ Array/66_Plus_One.py
@@ -12,24 +12,25 @@ class Solution:
         return digits
 
 
-# class Solution:
-#     def plusOne(self, digits: List[int]) -> List[int]:
-#         n = len(digits)
-#         digits[-1] += 1
-#         carry = digits[-1] > 9
-#         digits[-1] %= 10
-#
-#         index = -1
-#
-#         while carry and n > -index:
-#             index -= 1
-#
-#             digits[index] += 1
-#             carry = digits[index] > 9
-#             digits[index] %= 10
-#
-#         if carry:
-#             digits.insert(0, 1)
-#
-#         return digits
 
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        digits[-1] += 1
+        carry = digits[-1] > 9
+        digits[-1] %= 10
+
+        index = -1
+
+        while carry and n > -index:
+            index -= 1
+
+            digits[index] += 1
+            carry = digits[index] > 9
+            digits[index] %= 10
+
+        if carry:
+            digits.insert(0, 1)
+
+        return digits
