74a40f8df1106081c44b5d1db1b5b2fd0a20b23f
gabelchik <lensuperhero@gmail.com>
2025-07-28 23:33:28 +0300

    Trees

--- Trees/101_Symmetric_Tree.py
+++ Trees/101_Symmetric_Tree.py
@@ -1,13 +1,3 @@
-from typing import Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         def check_this_out(node1, node2):
@@ -21,9 +11,3 @@ class Solution:
             return check_this_out(node1.left, node2.right) and check_this_out(node2.left, node1.right)
 
         return check_this_out(root.left, root.right)
-
-
-solution = Solution()
-node = TreeNode(1, TreeNode(2, None, TreeNode(3)), TreeNode(2, None, TreeNode(3)))
-# node = TreeNode(1, TreeNode(2, TreeNode(3), TreeNode(4)), TreeNode(2, TreeNode(4), TreeNode(3)))
-print(solution.isSymmetric(node))
