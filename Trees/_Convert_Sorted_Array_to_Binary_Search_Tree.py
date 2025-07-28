e53618eb421bce847cfc2ec68e50398d7d8bcc02
gabelchik <lensuperhero@gmail.com>
2025-07-28 23:31:02 +0300

    Update _Convert_Sorted_Array_to_Binary_Search_Tree.py

--- Trees/_Convert_Sorted_Array_to_Binary_Search_Tree.py
+++ Trees/_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -1,41 +1,25 @@
-from typing import List, Optional
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        root1 = TreeNode(nums[0])
+        if len(nums) == 1:
+            return root1
+        for i in range(1, (len(nums)+1)//2):
+            new_node = TreeNode(nums[i])
+            new_node.left = root1
+            root1 = new_node
+        root2 = TreeNode(nums[(len(nums)+1)//2])
+        for i in range((len(nums)+3)//2, len(nums)):
+            new_node = TreeNode(nums[i])
+            new_node.left = root2
+            root2 = new_node
+        root1.right = root2
+        return root1
 
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 
-
-# class Solution:
-#     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-#         root1 = TreeNode(nums[0])
-#         if len(nums) == 1:
-#             return root1
-#         for i in range(1, (len(nums)+1)//2):
-#             new_node = TreeNode(nums[i])
-#             new_node.left = root1
-#             root1 = new_node
-#         root2 = TreeNode(nums[(len(nums)+1)//2])
-#         for i in range((len(nums)+3)//2, len(nums)):
-#             new_node = TreeNode(nums[i])
-#             new_node.left = root2
-#             root2 = new_node
-#         root1.right = root2
-#         return root1
 
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         if len(nums) == 0:
             return None
         return TreeNode(nums[len(nums)//2], self.sortedArrayToBST(nums[:len(nums)//2]), self.sortedArrayToBST(nums[len(nums)//2 + 1:]))
-
-
-
-solution = Solution()
-a = [-10,-3,0,5,9]
-node = solution.sortedArrayToBST(a)
-
-
-
