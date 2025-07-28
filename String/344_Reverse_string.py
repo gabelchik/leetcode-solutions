7e790a7c098116c7b0767f3707c7025146389329
gabelchik <lensuperhero@gmail.com>
2025-07-28 23:23:51 +0300

    Update 344_Reverse_string.py

--- String/344_Reverse_string.py
+++ String/344_Reverse_string.py
@@ -1,12 +1,7 @@
-from typing import List
-
-
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s.reverse()
-
-
-solution = Solution()
-s = ['h', 'i']
-solution.reverseString(s)
-print(s)
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
