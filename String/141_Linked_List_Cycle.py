8077ad2cf2472816e6fd30fd44a827ede7d62b74
gabelchik <lensuperhero@gmail.com>
2025-07-28 23:18:14 +0300

    Update 141_Linked_List_Cycle.py

--- String/141_Linked_List_Cycle.py
+++ String/141_Linked_List_Cycle.py
@@ -1,25 +1,18 @@
-from typing import Optional
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None or head.next is None:
+            return False
+
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast.next is None or fast.next.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
 
 
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-# class Solution:
-#     def hasCycle(self, head: Optional[ListNode]) -> bool:
-#         if head is None or head.next is None:
-#             return False
-#
-#         slow = head
-#         fast = head.next
-#         while slow != fast:
-#             if fast.next is None or fast.next.next is None:
-#                 return False
-#             slow = slow.next
-#             fast = fast.next.next
-#         return True
 
 
 class Solution:
@@ -32,8 +25,3 @@ class Solution:
             if slow != fast:
                 return True
         return False
-
-solution = Solution()
-head = ListNode(1)
-head.next = ListNode(2)
-print(solution.hasCycle(head))
