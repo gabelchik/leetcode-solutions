5d92973899ee2204f2408ec2d0e5e2c0655c6e5d
gabelchik <lensuperhero@gmail.com>
2025-07-28 23:05:59 +0300

    234_Palindrome Linked List.py

--- Linked List/234_Palindrome Linked List.py
+++ Linked List/234_Palindrome Linked List.py
@@ -1,11 +1,3 @@
-from typing import Optional
-
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         node, stack = head, []
@@ -18,33 +10,28 @@ class Solution:
                 return False
             node = node.next
         return True
-# class Solution:
-#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-#         half, end = head, head
-#         while end and end.next:
-#             half = half.next
-#             end = end.next.next
-#
-#         past, cur = None, half
-#         while cur:
-#             tmp = cur.next
-#             cur.next = past
-#             past = cur
-#             cur = tmp
-#
-#         first = head
-#         while past:
-#             if first.val != past.val:
-#                 return False
-#             past = past.next
-#             first = first.next
-#         return True
 
 
-solution = Solution()
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(3)
-head.next.next.next = ListNode(2)
-head.next.next.next.next = ListNode(1)
-print(solution.isPalindrome(head))
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        half, end = head, head
+        while end and end.next:
+            half = half.next
+            end = end.next.next
+
+        past, cur = None, half
+        while cur:
+            tmp = cur.next
+            cur.next = past
+            past = cur
+            cur = tmp
+
+        first = head
+        while past:
+            if first.val != past.val:
+                return False
+            past = past.next
+            first = first.next
+        return True
