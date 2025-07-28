4ef68b0421a96f4938e9c69befb7b42fb9c12e2b
gabelchik <lensuperhero@gmail.com>
2025-07-28 23:04:53 +0300

    206_Reverse_Linked_List.py

--- Linked List/206_Reverse_Linked_List.py
+++ Linked List/206_Reverse_Linked_List.py
@@ -1,20 +1,3 @@
-from typing import Optional
-
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
-def print_Node(head):
-    list = []
-    while head:
-        list.append(head.val)
-        head = head.next
-    print(*list)
-    return
-
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
@@ -28,10 +11,3 @@ class Solution:
             prev = tmp1
         cur.next = prev
         return cur
-
-head = ListNode
-# head = ListNode(1)
-# head.next = ListNode(2)
-solution = Solution()
-head = solution.reverseList(head)
-print_Node(head)
