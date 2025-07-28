c91ce60814685c6f71d53844ed46c80814d586ec
gabelchik <lensuperhero@gmail.com>
2025-07-28 23:06:28 +0300

    237_Delete_Node_in_a_Linked_List.py

--- Linked List/237_Delete_Node_in_a_Linked_List.py
+++ Linked List/237_Delete_Node_in_a_Linked_List.py
@@ -1,17 +1,3 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-def print_Node(head):
-    list = []
-    current_element = head
-    while current_element:
-        list.append(current_element.val)
-        current_element = current_element.next
-    print(*list)
-    return
-
 class Solution:
     def deleteNode(self, node):
         while node.next.next:
@@ -19,11 +5,3 @@ class Solution:
             node = node.next
         node.val = node.next.val
         node.next = None
-
-
-solution = Solution()
-head = ListNode(4)
-head.next = ListNode(5)
-head.next.next = ListNode(1)
-solution.deleteNode(head.next)
-print_Node(head)
